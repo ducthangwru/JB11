512e2ad38fadb2533f9902ca051094c63d48ba47
thang <ducthangwru97@gmail.com>
2016-06-22 23:46:14 +0700

    first commit

---
+++ Day2/src/Lesson4.java
@@ -0,0 +1,27 @@
+
+public class Lesson4 {
+    public static void main(String[] args) {
+        //khai báo và khởi tạo biến
+        int a = 4;
+        float b = (float) 3.4;
+        double c = (double) 2.34;
+        boolean d = true;
+        char e = 's';
+        long f = 2323;
+        short g = 422;
+        byte h = 21;
+
+        //khai báo hằng
+        final float PI = (float) 3.14;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("e = " + e);
+        System.out.println("f = " + f);
+        System.out.println("g = " + g);
+        System.out.println("h = " + h);
+        System.out.println("PI = " + PI);
+    }
+}
