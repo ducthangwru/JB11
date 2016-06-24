0fbcd87d57bbee5b96c78644944e99fb1070da9a
thang <ducthangwru97@gmail.com>
2016-06-24 19:59:40 +0700

    commit

--- Day3/src/Lesson1.java
+++ Day3/src/Lesson1.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Lesson1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -10,16 +11,17 @@ public class Lesson1 {
         Year = input.nextInt();
 
         if (Month == 2) {
-            if ((Year % 100 == 0 && Year/100 % 4 != 0))
+            if ((Year % 100 == 0 && Year / 100 % 4 != 0)) {
                 System.out.printf("Tháng 2 Năm %d có 28 ngày", Year);
-            else
+            } else {
                 System.out.printf("Tháng 2 Năm %d có 29 ngày", Year);
-        }
-        else{
-            if(Month == 4 || Month == 6 || Month == 9 || Month == 11)
+            }
+        } else {
+            if (Month == 4 || Month == 6 || Month == 9 || Month == 11) {
                 System.out.printf("Tháng %d Năm %d có 30 ngày", Month, Year);
-            else
+            } else {
                 System.out.printf("Tháng %d Năm %d có 31 ngày", Month, Year);
+            }
         }
     }
 }
--- Day3/src/Lesson2.java
+++ Day3/src/Lesson2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Lesson2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -14,26 +15,26 @@ public class Lesson2 {
         System.out.printf("Phương trình vừa nhập là: (%d)X^2 + (%d)X + (%d) = 0 \n", a, b, c);
         delta = (float) (Math.pow(b, 2) - 4 * a * c);
 
-        if(a == 0){
-            if(b == 0){
-                if(c == 0)
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
                     System.out.println("Phương trình vô số nghiệm");
-                else
+                } else {
                     System.out.println("Phương trình vô nghiệm");
-            }
-            else{
-                if(c == 0)
+                }
+            } else {
+                if (c == 0) {
                     System.out.println(" X = 0 ");
-                else
+                } else {
                     System.out.println("X = " + (-b / c));
+                }
             }
-        }
-        else{
-            if(delta < 0)
+        } else {
+            if (delta < 0) {
                 System.out.println("Phương trình vô nghiệm");
-            else if (delta == 0)
-                System.out.println("Phương trình có nghiệm X = " + (-b / 2*a));
-            else{
+            } else if (delta == 0) {
+                System.out.println("Phương trình có nghiệm X = " + (-b / 2 * a));
+            } else {
                 x1 = (float) ((-b + Math.sqrt(delta)) / (2 * a));
                 x2 = (float) ((-b - Math.sqrt(delta)) / (2 * a));
                 System.out.println("Phương trình có 2 nghiệm là: X1 = " + x1 + " và X2 = " + x2);
