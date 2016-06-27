1589a67e36ef38394623b39162a06bf12421eaff
thang <ducthangwru97@gmail.com>
2016-06-27 19:28:30 +0700

    commit

--- Day3/src/Lesson5.java
+++ Day3/src/Lesson5.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 public class Lesson5 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         int a, b;
         char choose;
@@ -12,17 +12,17 @@ public class Lesson5 {
         System.out.println("Nhập phép tính: ");
         choose = (char) System.in.read();
 
-        while(choose) {
-            case '+':
+        switch (choose) {
+            case 43:
                 System.out.println("Tổng 2 số là: " + (a + b));
                 break;
-            case '-':
+            case 45:
                 System.out.println("Hiệu 2 số là: " + (a - b));
                 break;
-            case '*':
+            case 42:
                 System.out.println("Tích 2 số là: " + (a * b));
                 break;
-            case '/':
+            case 47:
                 System.out.println("Thương 2 số là: " + (float) a / b );
             break;
         }
