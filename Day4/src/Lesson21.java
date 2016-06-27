411dca2779485b670ea7bf04fdc89f77379d1fc0
thang <ducthangwru97@gmail.com>
2016-06-27 18:36:18 +0700

    commit

---
+++ Day4/src/Lesson21.java
@@ -0,0 +1,15 @@
+public class Lesson21 {
+
+    public static void main(String [] args){
+        Lesson21 obj = new Lesson21();
+        obj.add(10, 10);
+        obj.add(10.5f, 10.24f);
+    }
+
+    public  static void add(int a, int b){
+        System.out.println(a + b);
+    }
+    public static void add(float a, float b){
+        System.out.println(a + b);
+    }
+}
