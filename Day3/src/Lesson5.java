8bcc6e9b78fca5a20289d4130555fdb3dcd1b0e4
thang <ducthangwru97@gmail.com>
2016-06-27 19:30:50 +0700

    commit

--- Day3/src/Lesson5.java
+++ Day3/src/Lesson5.java
@@ -25,6 +25,9 @@ public class Lesson5 {
             case 47:
                 System.out.println("Thương 2 số là: " + (float) a / b );
             break;
+            default:
+                System.out.println("Bạn nhập sai!");
+                break;
         }
     }
 }
